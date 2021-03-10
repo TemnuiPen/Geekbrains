1f468fcb4e93d034573db88fcc4270f446c24f85
Leha <maltsev0017@gmail.com>
2021-03-10 10:18:36 +0300

    Homework for lesson 7

---
+++ src/main/java/ru/geekbrains/lessons/Cat.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.lessons;
+
+public class Cat  {
+
+    private String name;
+    private int appetite;
+    boolean satiety;
+
+    public Cat(String name, int appetite, boolean satiety) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = satiety;
+    }
+
+    void eat(Plate p) {
+        p.decreaseFood(appetite);
+    }
+}
